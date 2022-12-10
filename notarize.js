1963e40efb851aea217e4526ba6ff47d4c19fb74
Joel Wetzell <jwetzell@yahoo.com>
2022-12-10 14:19:00 -0600

    cleanup notarize script

--- notarize.js
+++ notarize.js
@@ -7,27 +7,26 @@ module.exports = async function (params) {
         console.log('Only need to notarize MacOS, skipping')
         return
     }
-    console.log(params)
+    
     const appId = 'com.stagehacks.cueview'
 
     const appPath = path.join(
         params.appOutDir,
         `${params.packager.appInfo.productFilename}.app`
     )
+
     if (!fs.existsSync(appPath)) {
-        console.log('App file not found skipping notarization.');
+        console.error('App file not found skipping notarization.');
         return;
     }
-
-    console.log(
-        `Notarizing ${appId} found at ${appPath}`
-    )
+    
     if(process.env.APPLE_ID === undefined || process.env.APPLE_ID_PASSWORD === undefined){
         console.log('Apple ID and Password must be set in order to notarize.')
         return;
     }
 
     try {
+        console.log(`Notarizing ${appId} found at ${appPath}`)
         await notarize.notarize({
             appBundleId: appId,
             appPath,
@@ -36,6 +35,7 @@ module.exports = async function (params) {
         })
         console.log(`Done notarizing ${appId}`)
     } catch (error) {
+        console.log('There was an error notarizing.')
         console.error(error)
     }
 }
